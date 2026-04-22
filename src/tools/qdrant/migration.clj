c442e96e793be76351f7a48e2ac06852cd90f815
indy <kslvsunil@gmail.com>
2026-04-22 16:34:48 +0530

    remove unused qdrant payload indexes (action_name, safety, content); nothing queries with filters

--- src/tools/qdrant/migration.clj
+++ src/tools/qdrant/migration.clj
@@ -6,16 +6,9 @@
 
 (def vector-size 3072)
 
-(def payload-indexes
-  [{:field :action_name :schema-type :keyword}
-   {:field :safety      :schema-type :keyword}
-   {:field :content     :schema-type :text}])
-
 (defn run
   [client]
   (when-not (qdrant/collection-exists? client {:collection-name collection-name})
     (qdrant/create-collection client {:collection-name collection-name
                                       :vector-size     vector-size
-                                      :distance        :cosine}))
-  (doseq [idx payload-indexes]
-    (qdrant/create-payload-index client (assoc idx :collection-name collection-name))))
+                                      :distance        :cosine})))
