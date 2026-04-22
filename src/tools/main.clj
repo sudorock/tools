ec16b887ae2c7f07cd39ef5d52f7aaa0c51de5db
indy <kslvsunil@gmail.com>
2026-04-22 17:24:13 +0530

    log 'server started' after system init

--- src/tools/main.clj
+++ src/tools/main.clj
@@ -1,9 +1,11 @@
 (ns tools.main
   (:require
+   [glass.log :as log]
    [tools.system :as system]
    [tools.actions])
   (:gen-class))
 
 (defn -main
   [& _args]
-  (system/init))
+  (system/init)
+  (log/info {:message "server started"}))
